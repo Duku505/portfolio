57c0c4ac9efc2d3f85a7bbaccec5831819983670
Batdulguun Enkhtuya <dulguun_duku@live.com>
2024-06-25 05:42:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Correctly set to your repository name
+  base: '/portfolio/',
+  plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
