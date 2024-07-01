a943f66299dc67a6cb1cfb49b8c484104a5ae241
Duku505 <dulguun_duku@live.com>
2024-07-01 22:26:45 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Correctly set to your repository name
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-});
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js', 'jsx'],
+      'text/javascript': ['js', 'jsx']
+    }
+  }
+})
+
